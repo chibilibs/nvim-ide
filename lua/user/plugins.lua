17d97d53fd33b13c01207ab42211fb474fa7dd19
Guilherme Menezes <guilhermeladins@gmail.com>
2022-11-03 17:13:40 -0300

    fixup! Improved rust

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -81,21 +81,7 @@ return packer.startup(function(use)
 	})
 
 	-- rust plugins
-	use({
-		"simrat39/rust-tools.nvim",
-		config = function()
-			require("rust-tools").setup({
-				server = {
-					on_attach = function(_, bufnr)
-						-- Hover actions
-						vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-						-- Code action groups
-						vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
-					end,
-				},
-			})
-		end,
-	})
+	use({ "simrat39/rust-tools.nvim" })
 
 	-- Colorschemes
 	use({ "folke/tokyonight.nvim" })
